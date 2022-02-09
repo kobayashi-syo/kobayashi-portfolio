3990875cc94f98a093a8b13cc0ee64462aa6e0ff
Syo-Kobayashi <kobayashi@crunchtimer.jp>
2022-02-09 15:58:58 +0900

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kobayashi Portfolio</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+</head>
+
+<body>
+    <header class="l-header">
+
+    </header>
+    <h2 class="text-2xl">Hello World!!</h2>
+</body>
+
+</html>
